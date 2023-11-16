05ea22ec591f3e4d00807f2df7ebc230337b3d0d
Asif Kassam Ali <asif.k.dev@gmail.com>
2023-11-16 23:36:14 +0900

    project started

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/photos", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  let arr = await res.json();
+  return { arr };
+}
+
+const Blog = async () => {
+  const data = await getData();
+  return (
+    <div className={styles.mainContainer}>
+      {data.arr.slice(0, 10).map((item) => (
+        <Link
+          href={`/blog/${item.id}`}
+          className={styles.container}
+          key={item.id}
+        >
+          <div className={styles.imageContainer}>
+            <Image
+              src={item.url}
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            {/* <p className={styles.desc}>{item.desc}</p> */}
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Blog;
